8b8c1f689cf6d75c8ca2594a854f6610e03f82f8
Tharsis Lamin <tharsisboamorte@hotmail.com>
2024-06-21 13:57:12 -0300

    Feat - Added main screen and callback to biometricmanager

--- app/src/main/java/com/project/biometric_auth_app/MainActivity.kt
+++ app/src/main/java/com/project/biometric_auth_app/MainActivity.kt
@@ -1,47 +1,112 @@
 package com.project.biometric_auth_app
 
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import android.provider.Settings
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.biometric.BiometricManager.Authenticators.BIOMETRIC_STRONG
+import androidx.biometric.BiometricManager.Authenticators.DEVICE_CREDENTIAL
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.project.biometric_auth_app.BiometricPromptManager.BiometricResult
 import com.project.biometric_auth_app.ui.theme.BiometricauthappTheme
 
 class MainActivity : AppCompatActivity() {
+
+    private val promptManager by lazy {
+        BiometricPromptManager(this)
+    }
+
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             BiometricauthappTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                Scaffold(modifier = Modifier.fillMaxSize()) {
+                    val biometricResult by promptManager.promptResults.collectAsState(
+                        initial = null
                     )
+                    val enrollLauncher =
+                        rememberLauncherForActivityResult(
+                            contract = ActivityResultContracts.StartActivityForResult(),
+                        ) {
+                            println("Activity result: $it")
+                        }
+                    LaunchedEffect(biometricResult) {
+                        if (biometricResult is BiometricResult.AutheticationNotSet) {
+                            if (Build.VERSION.SDK_INT >= 30) {
+                                val enrollIntent = Intent(Settings.ACTION_BIOMETRIC_ENROLL).apply {
+                                    putExtra(
+                                        Settings.EXTRA_BIOMETRIC_AUTHENTICATORS_ALLOWED,
+                                        BIOMETRIC_STRONG or DEVICE_CREDENTIAL
+                                    )
+                                }
+                                enrollLauncher.launch(enrollIntent)
+                            }
+                        }
+                    }
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize(),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Button(onClick = {
+                            promptManager.showBiometricPrompt(
+                                title = "Sample prompt",
+                                description = "Sample prompt description"
+                            )
+                        }) {
+                            Text(text = "Authenticate")
+                        }
+                        biometricResult?.let { result ->
+                            Text(
+                                text = when (result) {
+                                    is BiometricResult.AuthenticationError -> {
+                                        result.error
+                                    }
+
+                                    BiometricResult.AuthenticationFailed -> {
+                                        "Authentication Failed"
+                                    }
+
+                                    BiometricResult.AutheticationNotSet -> {
+                                        "Authentication not set"
+                                    }
+
+                                    BiometricResult.AuthenticationSuccess -> {
+                                        "Authentication Success"
+                                    }
+
+                                    BiometricResult.FeatureUnavailable -> {
+                                        "Feature unavailable"
+                                    }
+
+                                    BiometricResult.HardwareUnavailable -> {
+                                        "Hardware unavailable"
+                                    }
+                                }
+                            )
+                        }
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    BiometricauthappTheme {
-        Greeting("Android")
     }
 }
