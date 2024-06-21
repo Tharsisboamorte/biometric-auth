1519c44bbb7fcc89f7fb1c9a6f55db38f3179ea7
Tharsis Lamin <tharsisboamorte@hotmail.com>
2024-06-21 13:35:08 -0300

    Feat - Added BiometricPromptManager class and dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,6 +54,8 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
+    implementation(libs.androidx.biometric)
+    implementation(libs.androidx.appcompat)
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
